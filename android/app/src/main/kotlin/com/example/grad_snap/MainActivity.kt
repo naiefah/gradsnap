6c035475362602987ebb885b96a63be9bb1c965c
naiefah <naiefahfawwaztiyani@gmail.com>
2026-06-10 00:01:38 +0700

    push shared preferences

---
+++ android/app/src/main/kotlin/com/example/grad_snap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.grad_snap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color primary = Color(0xFFD4AF37);
+
+  static const Color secondary = Color(0xFFF8D7DA);
+
+  static const Color white = Colors.white;
+
+  static const Color dark = Color(0xFF2D2D2D);
+
+}
